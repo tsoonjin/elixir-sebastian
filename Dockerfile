fcebb375c7f35cdd6ead910a7527ff181971fe14
jinified <jinified@gmail.com>
2021-02-21 20:14:25 +0800

    Modify Dockerfile and add MIX_ENV to circle step

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,7 @@ WORKDIR /app
 COPY mix.exs .
 COPY mix.lock .
 
+ARG MIX_ENV
+ENV MIX_ENV=$MIX_ENV
+
 CMD mix deps.get && mix phx.server
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -13,7 +13,7 @@ secret_key_base =
 
 config :sebastian, SebastianWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: {:system, "PORT"},
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
